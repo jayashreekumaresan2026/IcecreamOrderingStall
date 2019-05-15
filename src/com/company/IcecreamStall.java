f64f128ffdeb0edcaca40a8a71f799c364f15854
Jayashree <jayashree.kammu@gmail.com>
2019-05-15 18:42:21 +0530

    display Icecream and flavors

--- src/com/company/IcecreamStall.java
+++ src/com/company/IcecreamStall.java
@@ -3,17 +3,16 @@ package com.company;
 class MenuList {
     String name;
     String flavors;
-    double cost;
 
-    void getItemDetail(String name, String flavors, double cost) {
+    void getItemDetail(String name, String flavors) {
         this.name = name;
         this.flavors = flavors;
-        this.cost = cost;
+
 
     }
     void showMenuItems()
     {
-        System.out.println(name + "  | " + flavors + " |  " + cost + " |");
+        System.out.println("|"+name + "  | " + flavors + " |  " );
     }
 
 
@@ -24,13 +23,13 @@ public class IcecreamStall {
         int i;
         MenuList[] itemArray = new MenuList[3];
         MenuList itemList1 = new MenuList();
-        System.out.println("      ICECREAM MENU LIST     ");
+        System.out.println("      ICE CREAM MENU LIST     ");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        itemList1.getItemDetail("Stick", "Chocolate  ", 3);
+        itemList1.getItemDetail("Stick", " Chocolate  ");
         MenuList itemList2 = new MenuList();
-        itemList2.getItemDetail("Cone ", " Strawberry", 1);
+        itemList2.getItemDetail("Cone ", " Strawberry");
         MenuList itemList3 = new MenuList();
-        itemList3.getItemDetail("Cup  ", "  vanilla  ", 50);
+        itemList3.getItemDetail("Cup  ", "  vanilla  ");
         itemArray[0] = itemList1;
         itemArray[1] = itemList2;
         itemArray[2] = itemList3;
