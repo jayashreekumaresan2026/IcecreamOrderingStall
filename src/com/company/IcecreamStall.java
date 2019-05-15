25a61b5d537122d2a91c1763b8971ef62feea4ae
Jayashree <jayashree.kammu@gmail.com>
2019-05-15 21:21:41 +0530

    displayed sum of Type and flavour

--- src/com/company/IcecreamStall.java
+++ src/com/company/IcecreamStall.java
@@ -3,18 +3,31 @@ package com.company;
 class MenuList {
     String name;
     String flavors;
+    double iceCreamCost;
+    double flavourCost;
 
-    void getItemDetail(String name, String flavors) {
+    void getItemDetail(String name, double iceCreamCost, String flavors, double flavourCost) {
         this.name = name;
         this.flavors = flavors;
-
+        this.iceCreamCost = iceCreamCost;
+        this.flavourCost = flavourCost;
 
     }
-    void showMenuItems()
-    {
-        System.out.println("|"+name + "  | " + flavors + " |  " );
+
+    void showMenuItems() {
+        System.out.println("|" + name + "  | " + iceCreamCost + " |  " + flavors + "  | " + flavourCost);
     }
 
+
+}
+
+class SumTypeAndflavoursCost {
+    void sumTypeAndflavoursCost(MenuList menuList) {
+        double typeandflavourcost;
+        typeandflavourcost = menuList.iceCreamCost + menuList.flavourCost;
+        System.out.println(typeandflavourcost);
+
+    }
 
 }
 
@@ -25,17 +38,24 @@ public class IcecreamStall {
         MenuList itemList1 = new MenuList();
         System.out.println("      ICE CREAM MENU LIST     ");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        itemList1.getItemDetail("Stick", " Chocolate  ");
+        itemList1.getItemDetail("Stick", 20, "chocolate", 5);
         MenuList itemList2 = new MenuList();
-        itemList2.getItemDetail("Cone ", " Strawberry");
+        itemList2.getItemDetail("Cone ", 30, "strawberry", 9);
         MenuList itemList3 = new MenuList();
-        itemList3.getItemDetail("Cup  ", "  vanilla  ");
+        itemList3.getItemDetail("Cup  ", 25, "vanilla", 15);
         itemArray[0] = itemList1;
         itemArray[1] = itemList2;
         itemArray[2] = itemList3;
         for (i = 0; i < (itemArray.length); i++) {
             itemArray[i].showMenuItems();
         }
+        SumTypeAndflavoursCost object1 = new SumTypeAndflavoursCost();
+        System.out.println();
+        System.out.println("Sum cost each of itemType and flavour");
+        object1.sumTypeAndflavoursCost(itemList1);
+        object1.sumTypeAndflavoursCost(itemList2);
+        object1.sumTypeAndflavoursCost(itemList3);
 
     }
 }
+
